aabf338ffb7a4d580676e792bbe935c681731e8c
vstepanuga <v.stepanuga@gmai.com>
2019-12-09 16:00:31 -0700

    Added make_move, evaluate_move, etc.

--- vstepanuga-jmiller-bot.py
+++ vstepanuga-jmiller-bot.py
@@ -1,3 +1,6 @@
+import copy
+
+
 class OthelloBot:
     def __init__(self, board_size):
         self.board = []
@@ -10,12 +13,19 @@ class OthelloBot:
                 temp.append('-')
             self.board.append(temp)
 
-    def make_move(self, x, y, color='W'):
-        if self.is_valid_move(x, y, color):
-            temp_board = list(tuple(self.board))
-            temp_board[x][y] = color
-            cost = self.evaluate_board_cost(temp_board)
-            return [cost, temp_board]
+    # Function responsible for making the most powerful move in the 2 second span.
+    # Returns a move: (Color, (X, Y))
+    def make_move(self):
+        legal_moves = self.get_all_moves(self.board)
+        for move in legal_moves:
+            self.evaluate_move(move[1][0], move[1][1], move[0])
+
+    # Function which counts the move cost
+    def evaluate_move(self, x, y, color='W'):
+        temp_board = copy.deepcopy(self.board)
+        temp_board[x][y] = color
+        cost = self.evaluate_board_cost(temp_board)
+        return [cost, temp_board]
 
     def is_valid_move(self, x, y, color):
         return True
@@ -23,7 +33,9 @@ class OthelloBot:
     def evaluate_board_cost(self, temp_board):
         return 1
 
+    def get_all_moves(self, board):
+        return ['B', (4, 5)]
+
 
 if __name__ == '__main__':
     test = OthelloBot(8)
-    print(test.make_move(3, 5))
