4f5b0919fa1c278d09fa83f5357fae24e7d428fc
vstepanuga <v.stepanuga@gmai.com>
2019-12-09 15:38:35 -0700

    return some sort of board state with cost function

--- vstepanuga-jmiller-bot.py
+++ vstepanuga-jmiller-bot.py
@@ -10,6 +10,20 @@ class OthelloBot:
                 temp.append('-')
             self.board.append(temp)
 
+    def make_move(self, x, y, color='W'):
+        if self.is_valid_move(x, y, color):
+            temp_board = list(tuple(self.board))
+            temp_board[x][y] = color
+            cost = self.evaluate_board_cost(temp_board)
+            return [cost, temp_board]
+
+    def is_valid_move(self, x, y, color):
+        return True
+
+    def evaluate_board_cost(self, temp_board):
+        return 1
+
+
 if __name__ == '__main__':
     test = OthelloBot(8)
-    print(test.board)
+    print(test.make_move(3, 5))
