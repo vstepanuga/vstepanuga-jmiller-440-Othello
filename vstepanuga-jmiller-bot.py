a17f6746e36072b153e53b7dea69d2ac5e22197b
vstepanuga <v.stepanuga@gmai.com>
2019-12-09 16:30:57 -0700

    Fixed make_move to return appropriate tuple

--- vstepanuga-jmiller-bot.py
+++ vstepanuga-jmiller-bot.py
@@ -17,25 +17,31 @@ class OthelloBot:
     # Returns a move: (Color, (X, Y))
     def make_move(self):
         legal_moves = self.get_all_moves(self.board)
+        evaluated_moves = []
+
         for move in legal_moves:
-            self.evaluate_move(move[1][0], move[1][1], move[0])
+            evaluated_moves.append([move, self.evaluate_move(move[1][0], move[1][1], move[0])])
+
+        return tuple(max(evaluated_moves, key=lambda item: item[0])[0])
 
     # Function which counts the move cost
     def evaluate_move(self, x, y, color='W'):
         temp_board = copy.deepcopy(self.board)
         temp_board[x][y] = color
-        cost = self.evaluate_board_cost(temp_board)
-        return [cost, temp_board]
+        cost = self.evaluate_board_cost(temp_board, color)
+        return cost
 
     def is_valid_move(self, x, y, color):
         return True
 
-    def evaluate_board_cost(self, temp_board):
+    def evaluate_board_cost(self, temp_board, color):
         return 1
 
+    # Returns a list of all legal moves for a given board state
     def get_all_moves(self, board):
-        return ['B', (4, 5)]
+        return [['B', (4, 5)]]
 
 
 if __name__ == '__main__':
     test = OthelloBot(8)
+    print(test.make_move())
